ebb4dcaa5bf79cf4ff14b8f8c6af35e15d54891f
Andrew Li <andrew.li1987@yandex.com>
2016-09-20 15:51:27 +0800

    listing design

--- userowner/Listing.aspx.cs
+++ userowner/Listing.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class userowner_Listing : System.Web.UI.Page
+public partial class userowner_Listing : CommonPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
